ffaaa57054da76d64e97bc8e68a3c7eaeafd9dbe
anhnd <anhnd350309@gmail.com>
2023-08-25 16:34:36 +0700

    Feature: build

--- app/core/config.py
+++ app/core/config.py
@@ -1,56 +1,43 @@
 import json
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME = os.getenv('PROJECT_NAME', 'Lead management api')
+
+    PROJECT_NAME: str = os.getenv('PROJECT_NAME', 'Lead management api')
     DEBUG: bool = os.getenv('DEBUG', 'False')
-    SECRET_KEY = os.getenv('SECRET_KEY', '')
-    ALGORITHM = os.getenv('ALGORITHM', '')
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-    BACKEND_CORS_ORIGINS = ['*']
-    ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 7  # Token expired after 7 days
-    SECURITY_ALGORITHM = 'HS256'
-    LOGGING_CONFIG_FILE = os.path.join(BASE_DIR, 'logging.ini')
-
-    APP_ID = "LM"
-
-    DOMAIN = os.getenv("DOMAIN")
+    SECRET_KEY: Optional[str] = os.getenv('SECRET_KEY', '')
+    ALGORITHM: Optional[str] = os.getenv('ALGORITHM', '')
+    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[str] = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    BACKEND_CORS_ORIGINS: Optional[list] = ["*"]
+    ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * \
+        60 * 24 * 7  # Token expired after 7 days
+    SECURITY_ALGORITHM: Optional[str] = 'HS256'
+    LOGGING_CONFIG_FILE: Optional[str] = os.path.join(BASE_DIR, 'logging.ini')
 
     # db
-    DATABASE_URL = os.getenv('SQL_DATABASE_URL', 'sqlite:///.db')
+    DATABASE_URL: Optional[str] = os.getenv(
+        'SQL_DATABASE_URL', 'sqlite:///.db')
 
     # s3
-    MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT', '')
-    MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY', '')
-    MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY', '')
-    BUCKET_NAME = os.getenv('BUCKET_NAME', '')
+    MINIO_ENDPOINT: Optional[str] = os.getenv('MINIO_ENDPOINT', '')
+    MINIO_ACCESS_KEY: Optional[str] = os.getenv('MINIO_ACCESS_KEY', '')
+    MINIO_SECRET_KEY: Optional[str] = os.getenv('MINIO_SECRET_KEY', '')
+    BUCKET_NAME: Optional[str] = os.getenv('BUCKET_NAME', '')
 
     # Storage local
-    FS_DIRECTORY = os.getenv("FS_DIRECTORY", "/tmp")
+    FS_DIRECTORY: Optional[str] = os.getenv("FS_DIRECTORY", "/tmp")
 
     # RAM
     KAFKA_CONN_STR: str = os.getenv('KAFKA_CONN_STR')
-
-    GOOGLE_CREDENTIALS = json.loads(os.getenv('GOOGLE_CREDENTIAL'))
-
-    # TWILIO
-    ACCOUNT_SID_TWILIO: str = os.getenv("ACCOUNT_SID_TWILIO")
-    AUTH_TOKEN_TWILIO: str = os.getenv("AUTH_TOKEN_TWILIO")
-
-    BOOKING_CREATE_TOPIC: str = os.getenv('BOOKING_CREATE_TOPIC', 'booking')
-    EVENT_UPDATE_STATUS_TOPIC: str = os.getenv('EVENT_UPDATE_STATUS_TOPIC', 'event-update-status')
-    PAYMENT_TOPIC: str = os.getenv('PAYMENT_TOPIC', 'payment')
-
-    CONSUME_EVENT_UPDATE_STATUS: str = os.getenv("CONSUME_EVENT_UPDATE_STATUS", 'event-update-status-consumer')
-    CONSUME_BOOKING_CREATE: str = os.getenv("CONSUME_BOOKING_CREATE", 'booking-create-consume-group')
-    CONSUME_PAYMENT: str = os.getenv("CONSUME_PAYMENT", 'payment-consume')
 
 
 settings = Settings()
